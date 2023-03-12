79b5a3960c4bcb37fff391557083801fcc000c4a
josipprgic <josip.prgic98@gmail.com>
2023-03-12 22:05:48 +0100

    migrate app to new conway implementation
    Closes #10

--- jpcd-yew/src/main.rs
+++ jpcd-yew/src/main.rs
@@ -1,208 +1,109 @@
 use gloo::timers::callback::Interval;
 use yew::html::Scope;
 use yew::{classes, html, Component, Context, Html};
-
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
-
-impl Cell {
-    fn toggle(&mut self) {
-        *self = match *self {
-            Cell::Dead => Cell::Alive,
-            Cell::Alive => Cell::Dead,
-        };
-    }
-}
+use jpcd_game_of_life::{cell::Cell, universe::Universe};
 
 pub enum Msg {
     Start,
     Stop,
     Reset,
-    ToggleCell(usize),
+    ToggleCell((usize, usize)),
     Tick
 }
 
-pub struct Universe {
-    width: usize,
-    height: usize,
-    cells: Vec<Cell>,
+pub struct UniverseDec {
+    universe: Universe,
     active: bool,
-    _interval: Interval,
+    _interval: Option<Interval>,
 }
 
-impl Universe {
-
-    fn set_width(&mut self, size: usize) {
-        self.width = size;
-    }
-
-    fn set_height(&mut self, size: usize) {
-        self.height = size;
-    }
-
-    fn get_index(&self, row: usize, column: usize) -> usize {
-        (row * self.width + column) as usize
-    }
-
-    fn live_neighbor_count(&self, row: usize, column: usize) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-        count
-    }
-
-    fn get_cells(&self) -> &[Cell] {
-        &self.cells
-    }
-
-    fn set_cells(&mut self, cells: &[(usize, usize)]) {
-        self.reset();
-        for (row, col) in cells {
-            if *row < self.height && *col < self.width {
-                let idx = self.get_index(*row, *col);
-                self.cells[idx] = Cell::Alive
-            }
-        }
-    }
-
-    fn reset(&mut self) {
+impl UniverseDec {
+    fn new(interval: Option<Interval>) -> Self {
         let width = 53;
         let height = 40;
+        let mut universe = Universe::new(width, height);
 
-        self.cells = (0..width * height)
-            .map(|i| {
-                if i % 9 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
+        (0..(width * height))
+            .filter(|i| {
+                i % 9 == 0 || i % 7 == 0
             })
-            .collect();
-    }
+            .for_each(|id| {
+                universe.toggle(id % width, id / width)
+            });
 
-    pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
-
-                let next_cell = match (cell, live_neighbors) {
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    (Cell::Dead, 3) => Cell::Alive,
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
-            }
-        }
-
-        let mut diff = Vec::new();
-        for i in 0..next.len() {
-            if next[i] != self.cells[i] {
-                diff.push(i);
-            }
-        }
-        // log!{"Changed cells are {:?}", diff}
-        self.cells = next;
-    }
-
-    pub fn new(int: Interval) -> Universe {
-        let width = 53;
-        let height = 40;
-
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 9 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
-        Universe {
-            width,
-            height,
-            cells,
+        UniverseDec{
+            universe,
             active: false,
-            _interval: int
+            _interval: interval
         }
     }
 
-    pub fn width(&self) -> usize {
-        self.width
-    }
-
-    pub fn height(&self) -> usize {
-        self.height
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
-
-    pub fn toggle_cell(&mut self, row: usize, column: usize) {
-        let idx = self.get_index(row, column);
-        self.cells[idx].toggle();
-    }
-
-    fn view_cell(&self, offset: usize, cell: Cell, link: &Scope<Self>) -> Html {
+    fn view_cell(&self, width: usize, height: usize, cell: Cell, link: &Scope<Self>) -> Html {
         match cell {
             Cell::Dead => html! {
-            <div key={offset} class={classes!("game-cell", "dead-cell")}
-                onclick={link.callback(move |_| Msg::ToggleCell(offset))}>
+            <div key={width * height} class={classes!("game-cell", "dead-cell")}
+                onclick={link.callback(move |_| Msg::ToggleCell((width, height)))}>
             </div>
             },
             Cell::Alive => html! {
-            <div key={offset} class={classes!("game-cell", "alive-cell")}
-                onclick={link.callback(move |_| Msg::ToggleCell(offset))}>
+            <div key={width * height} class={classes!("game-cell", "alive-cell")}
+                onclick={link.callback(move |_| Msg::ToggleCell((width, height)))}>
             </div>
             }
         }
     }
 }
 
-impl Component for Universe {
+impl Component for UniverseDec {
     type Message = Msg;
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
         let callback = ctx.link().callback(|_| Msg::Tick);
-        let interval = Interval::new(1, move || callback.emit(()));
+        let interval = Interval::new(100, move || callback.emit(()));
 
-        Universe::new(interval)
+        UniverseDec::new(Some(interval))
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Start => {
+                self.active = true;
+                log::info!("Start");
+                false
+            }
+            Msg::Reset => {
+                self.universe.reset();
+                log::info!("Reset");
+                true
+            }
+            Msg::Stop => {
+                self.active = false;
+                log::info!("Stop");
+                false
+            }
+            Msg::ToggleCell(idx) => {
+                self.universe.toggle(idx.0, idx.1);
+                true
+            }
+            Msg::Tick => {
+                if self.active {
+                    self.universe.tick();
+                    true
+                } else {
+                    false
+                }
+            }
+        }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let cell_rows = self.cells
-            .chunks(self.width as usize)
-            .enumerate()
+        let cell_rows = self.universe.enumerate_rows()
             .map(|(n, cells)| {
-                let idx_offset = n * self.width;
-
                 let cells = cells
                     .iter()
                     .enumerate()
-                    .map(|(x, cell)| self.view_cell(idx_offset + x, *cell, ctx.link()));
+                    .map(|(x, cell)| self.view_cell(x, n, *cell, ctx.link()));
                 html! {
                         <div key={n} class="game-row">
                             { for cells }
@@ -222,9 +123,12 @@ impl Component for Universe {
                             { for cell_rows }
                         </div>
                         <div class="game-buttons">
-                            <button class="game-button" onclick={ctx.link().callback(|_| Msg::Start)}>{ "Start" }</button>
-                            <button class="game-button" onclick={ctx.link().callback(|_| Msg::Stop)}>{ "Stop" }</button>
-                            <button class="game-button" onclick={ctx.link().callback(|_| Msg::Reset)}>{ "Reset" }</button>
+                            <button class="game-button"
+                                onclick={ctx.link().callback(|_| Msg::Start)}>{ "Start" }</button>
+                            <button class="game-button"
+                                onclick={ctx.link().callback(|_| Msg::Stop)}>{ "Stop" }</button>
+                            <button class="game-button"
+                                onclick={ctx.link().callback(|_| Msg::Reset)}>{ "Reset" }</button>
                         </div>
                     </section>
                 </section>
@@ -236,43 +140,25 @@ impl Component for Universe {
             </div>
         }
     }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::Start => {
-                self.active = true;
-                log::info!("Start");
-                false
-            }
-            Msg::Reset => {
-                self.reset();
-                log::info!("Reset");
-                true
-            }
-            Msg::Stop => {
-                self.active = false;
-                log::info!("Stop");
-                false
-            }
-            Msg::ToggleCell(idx) => {
-                let cell = self.cells.get_mut(idx).unwrap();
-                cell.toggle();
-                true
-            }
-            Msg::Tick => {
-                if self.active {
-                    self.tick();
-                    true
-                } else {
-                    false
-                }
-            }
-        }
-    }
 }
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     log::trace!("Initializing yew...");
-    yew::Renderer::<Universe>::new().render();
+    yew::Renderer::<UniverseDec>::new().render();
+}
+
+#[test]
+fn test_one() {
+    let mut un = UniverseDec::new(None);
+
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
+    un.universe.tick();
 }
